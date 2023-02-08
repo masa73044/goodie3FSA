994ae637c1d16c3efa512bddb279188894abc26f
masa73044 <masa7304@gmail.com>
2023-02-08 18:10:37 +0700

    new

---
+++ app/components/allcandies.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Root = () => {
+  return (
+    <div>
+      <nav>
+        Goodie Bag
+        {/* <Link to="/">home</Link> */}
+      </nav>
+      <main>
+        <h1>Welcome to the Goodie Bag!</h1>
+        <p>What a nice home page for your goodies!</p>
+        {/* <Router>
+            <Route path="/api/candies" component={allcandies} />
+          </Router> */}
+      </main>
+    </div>
+  );
+};
+
+export default Root;
